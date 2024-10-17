914c4b4c5089ca4dcb3ab14ba68e1d52829b552d
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2024-10-17 13:22:38 +0530

    Create dictionary_basic.py

---
+++ Dictionary/dictionary_basic.py
@@ -0,0 +1,5 @@
+vector = {1:'Scalar', 2:'Vector', 3:'Tensor'}
+print(f"The whole dictionary: {vector}")
+print(f"A single element: {vector[1]}")
+print(f"When a matrix have 2 elements element: {vector[2]}")
+print(f"When a matrix have more than 2 elements element: {vector[3]}")
